2b5e28723916730e9b99ced35ed1254ca1596421
WizzardMaker <wizzardmaking@gmail.com>
2016-03-25 10:44:56 +0100

    FirstUpload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	public float distance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+
+		transform.position = target.transform.position - transform.forward * distance;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = Vector3.zero;
+
+		velocity += transform.forward * speed * Input.GetAxis("Vertical");
+		velocity += transform.right * speed * Input.GetAxis("Horizontal");
+
+		cc.SimpleMove(velocity);
+	}
+}
